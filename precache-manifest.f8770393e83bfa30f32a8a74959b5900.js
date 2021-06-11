df3506ec88d5dd89d816a1e5b452bb58af3c2c9e
Samagun <67420501+Samagun@users.noreply.github.com>
2021-06-11 12:31:53 +0600

    Updates

---
+++ precache-manifest.f8770393e83bfa30f32a8a74959b5900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e10c8eaa0fc72b509978a75f409c4c5",
+    "url": "/firstPtoject/index.html"
+  },
+  {
+    "revision": "318b0f32857111677c0b",
+    "url": "/firstPtoject/static/css/main.7d86e887.chunk.css"
+  },
+  {
+    "revision": "e1307af73e11b82966e8",
+    "url": "/firstPtoject/static/js/2.9e39d926.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/firstPtoject/static/js/2.9e39d926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318b0f32857111677c0b",
+    "url": "/firstPtoject/static/js/main.d181c16e.chunk.js"
+  },
+  {
+    "revision": "8ffd427e5de5f0684819",
+    "url": "/firstPtoject/static/js/runtime-main.f7ab5f36.js"
+  }
+]);
